f7b3c15f17e8096b492574e90cae97db07df1fca
Rebekah Bell <bellr@mit.edu>
2017-12-08 01:18:13 -0500

    cloud bug fix

--- scripts/hope.js
+++ scripts/hope.js
@@ -11,53 +11,53 @@ hopeResponses.forEach(function(resp) {
   var hopeCount = splitHopeData.filter(x => x.trim() === resp).length;
   cloudData.push({'text': resp, 'size': hopeCount});
 });
-// console.log(cloudData);
 
 // Set-up word cloud
 // check that word cloud does not already exist. If so, remove.
-if ($('.wordcloud') !== undefined) {
-	$('.wordcloud').remove();
+if ($('#hope-cloud-svg') !== undefined) {
+	$('#hope-cloud-svg').remove();
 }
 
 var color = d3.scale.linear()
         .domain([d3.min(cloudData, d => d.size), d3.max(cloudData, d => d.size)])
-        .range(["#ddd", "#222"]);
-        //.range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
-        //.range(["#222", "#333", "#444", "#555", "#666", "#777", "#888", "#999", "#aaa", "#bbb", "#ccc", "#ddd"]);
+        .range(["#222", "#ddd"]);
 
 var fontScale = d3.scale.linear()
   .domain([d3.min(cloudData, d => d.size), d3.max(cloudData, d => d.size)])
-  .range([12, 32]);
+  .range([20, 42]);
 
 var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<div class='word-tip'><strong>"+ d.text + "</strong> <span>(" + d.size + ")</span></div>";
+    return "<div class='word-tip'><strong>Count: </strong> <strong>" + Math.round(fontScale.invert(d.size)) + "</span></div>";
   });
 
-d3.layout.cloud().size([800, 800])
+d3.layout.cloud().size([800, 400])
         // .words(cloudData)
         // .rotate(0)
         // .fontSize(function(d) { return d.size; })
         // .on("end", draw)
         // .start();
 
-        .timeInterval(20)
+        //.timeInterval(20)
         .words(cloudData)
         .rotate(0)
         .spiral("archimedean")
-        .fontSize(function(d) { return fontScale(d.size); })
+        .fontSize(function(d) {
+          // console.log('cloud!');
+          // console.log(fontScale(d.size));
+          return fontScale(d.size);
+        })
         .text(function(d) { return d.text; })
-        //.rotate(function() { return ~~(Math.random() * 2) * 90; })
-        //.font("Impact")
         .on("end", draw)
         .start();
 
 function draw(words) {
   d3.select("#hope-cloud").append("svg")
     .attr("width", 800)
-    .attr("height", 800)
+    .attr("height", 400)
+    .attr("id", "hope-cloud-svg")
     .attr("class", "wordcloud")
     .append("g")
     // without the transform, words would get cutoff to the left and top, they would
@@ -67,7 +67,9 @@ function draw(words) {
     .selectAll("text")
     .data(words)
     .enter().append("text")
-    .style("font-size", function(d) { return fontScale(d.size) + "px"; })
+    .style("font-size", function(d) {
+      return d.size + "px";
+    })
     .style("fill", function(d, i) { return color(i); })
     .attr("transform", function(d) {
         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
